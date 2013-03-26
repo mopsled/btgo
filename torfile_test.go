aeae855a8280ad17e3666d6da128b4521ceabef2
Alec Geatches <throwpi@me.com>
2013-03-25 18:08:14 -0600

    Added multiple-file torrent file to test in torfile_test.go

--- torfile_test.go
+++ torfile_test.go
@@ -9,6 +9,9 @@ import (
 func TestNewTorfile(t *testing.T) {
 	// One-file torrent
 	testUbuntuTorrent(t)
+
+	// Multi-file torrent
+	testMultitracksTorrent(t)
 }
 
 func testUbuntuTorrent(t *testing.T) {
@@ -23,7 +26,7 @@ func testUbuntuTorrent(t *testing.T) {
 		t.Fatalf("Failed to parse test file %s: %s", file, err)
 	}
 
-	if !sameSlice(tfile.announceList, []string{"http://torrent.ubuntu.com:6969/announce", "http://ipv6.torrent.ubuntu.com:6969/announce"}) {
+	if !sameSlice(tfile.announceList, [][]string{[]string{"http://torrent.ubuntu.com:6969/announce"}, []string{"http://ipv6.torrent.ubuntu.com:6969/announce"}}) {
 		t.Errorf("Wrong announce list for %s: %s", file, tfile.announceList)
 	}
 
@@ -44,5 +47,30 @@ func testUbuntuTorrent(t *testing.T) {
 
 	if len(tfile.pieces) != int(math.Ceil(float64(firstFile.length)/float64(tfile.pieceLength))) {
 		t.Errorf("Wrong size of piece slice for %s: %d", file, len(tfile.pieces))
+	}
+}
+
+func testMultitracksTorrent(t *testing.T) {
+	file := "test/multitracks.torrent"
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.Fatalf("Failed to open test file %s", file)
+	}
+
+	tfile, err := NewTorfile(content)
+	if err != nil {
+		t.Fatalf("Failed to parse test file %s: %s", file, err)
+	}
+
+	if !sameSlice(tfile.announceList, [][]string{[]string{"http://tracker001.clearbits.net:7070/announce"}}) {
+		t.Errorf("Wrong announce list for %s: %s", file, tfile.announceList)
+	}
+
+	if tfile.pieceLength != 262144 {
+		t.Errorf("Wrong pieceLength for %s: %d", file, tfile.pieceLength)
+	}
+
+	if len(tfile.files) != 6 {
+		t.Fatalf("Wrong number of files for %s: %d", file, len(tfile.files))
 	}
 }
